d2a2dc8b9909b2a855dec2566434a9e5d7b896eb
William Gough <williamhenrygough@gmail.com>
2020-10-07 17:37:19 +0100

    chore: initial setup

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+//! This is a nicely document "Hello World" project.
+//! The runnable code in this project looks like this:
+//! ```
+//! Hello, World
+//! ```
+
+/// The `main()` function outputs "Hello, World!". This function is called
+/// automatically when the `hello` binary is run via:
+/// ```
+/// cargo run
+/// ```
+fn main() {
+    println!("Hello, world!");
+}
