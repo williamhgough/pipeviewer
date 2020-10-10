cdaaa30c958d8e35c5cd24945459f8697a4e305d
William Gough <williamhenrygough@gmail.com>
2020-10-10 11:39:33 +0100

    feat: refactor to crossbeam

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use crossbeam::channel::{bounded, unbounded};
 use pipeviewer::{args::Args, read, stats, write};
 use std::io::Result;
-use std::sync::mpsc;
 use std::thread;
 
 fn main() -> Result<()> {
@@ -11,11 +11,11 @@ fn main() -> Result<()> {
         silent,
     } = args;
 
-    let (stats_tx, stats_rx) = mpsc::channel::<Vec<u8>>();
-    let (writer_tx, writer_rx) = mpsc::channel::<Vec<u8>>();
+    let (stats_tx, stats_rx) = unbounded();
+    let (writer_tx, writer_rx) = bounded(1024);
 
-    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx));
-    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx, writer_tx));
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx, writer_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(silent, stats_rx));
     let write_handle = thread::spawn(move || write::write_loop(&outfile, writer_rx));
 
     // crash main thread if any threads crash
